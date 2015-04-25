36409fef27fa9b690d8072bb738c51edc833faa6
felixbarring <felix_barring@hotmail.com>
2015-04-26 01:05:43 +0200

    Simple Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <glfw3.h>
+GLFWwindow* window;
+#include <glm/glm.hpp>
+using namespace glm;
 
-main()
+int main( void )
 {
-  std::cout<<"Hello World \n \n";
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		return -1;
+	}
 
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	window = glfwCreateWindow( 800, 600, "Music Game", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	glClearColor(0.2f, 0.22f, 0.2f, 0.0f);
+
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0)
+	{
+		// Draw nothing, see you in tutorial 2 !
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
