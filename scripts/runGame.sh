f0ce3fbd1aa9fe5f41f8b55ddabfbf5da5a45fb7
felixbarring <felix_barring@hotmail.com>
2015-04-26 12:22:04 +0200

    New Main and build scripts

---
+++ scripts/runGame.sh
@@ -0,0 +1,3 @@
+cd ../build
+./MusicGame
+cd ../scripts
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,102 @@
+
+//#include "model/level.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
 #include <GL/glew.h>
 #include <glfw3.h>
-GLFWwindow* window;
 #include <glm/glm.hpp>
+
+GLFWwindow *window;
+
 using namespace glm;
 
 int main( void )
 {
-	if( !glfwInit() )
-	{
+	// Initialise GLFW
+	if( !glfwInit() ) {
 		fprintf( stderr, "Failed to initialize GLFW\n" );
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_SAMPLES, 16);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	window = glfwCreateWindow( 800, 600, "Music Game", NULL, NULL);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( 800, 600, "Game", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window.\n" );
+		glfwTerminate();
+		return -1;
+	}
 
 	glfwMakeContextCurrent(window);
 
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	glClearColor(0.2f, 0.22f, 0.2f, 0.0f);
+	const double fps = 60;
+	const double timePerFram = 1.0 / fps;
+	const double mili = 0.001;
+	std::chrono::milliseconds oneMilliSecond( 1 );
 
-	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0)
-	{
-		// Draw nothing, see you in tutorial 2 !
+	//Level level;
+
+	glClearColor(0.2f, 0.22f, 0.2f, 1.0f);
+
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 ){
+
+		double frameStartTime = glfwGetTime();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//level.render( glfwGetKey(window, GLFW_KEY_SPACE ) == GLFW_PRESS);
+
+		// Sleep the thread if we are running fast to save computer power.
+		// If we are running too fast and there is more than 1 miliseconds left until
+		//  next frame then the thread will sleep for the time that remains.
+		// If there is less than 2 milliseconds left, we busy wait until it is time to proceed with the next frame.
+		// The busy waiting is used to increase the timing accuracy, we do not want to start the next frame to late.
+		double totalFrameTime = glfwGetTime() - frameStartTime;
+		while(totalFrameTime < timePerFram){
+			double timeRemainingInMilis = (timePerFram - totalFrameTime) / mili;
+			if (timeRemainingInMilis > 2){
+				std::this_thread::sleep_for( (timeRemainingInMilis-1) * oneMilliSecond );
+			} else if(timeRemainingInMilis > 1){
+				std::this_thread::sleep_for( oneMilliSecond );
+			}
+			totalFrameTime = glfwGetTime() - frameStartTime;
+		}
+
+		// Printing mouse location
+		/*
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		std::cout << "XPOS: " << xpos << " YPOS: " << ypos;
+		*/
+
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
+
 	return 0;
 }
+
+
+
+
